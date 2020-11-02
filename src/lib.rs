9b2d10288d7f9e52dd05b1e8597fad7d49a4fad6
Simon <simon@systemli.org>
2020-11-02 07:36:32 +0100

    Added ColorBlindTypes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// Some magic incantations to make the compiler more pedantic (like --pedantic)
+#![deny(
+	future_incompatible,
+	nonstandard_style,
+	rust_2018_idioms,
+	missing_copy_implementations,
+	trivial_numeric_casts,
+	unsafe_code
+)]
+
 #[cfg(not(debug_assertions))]
 #[macro_use]
 extern crate human_panic;
@@ -11,4 +21,15 @@ fn main() {
 
 	// Setup Logging
 	// log::setup_logging()?;
+}
+
+enum ColorBlindType {
+	Achromatomaly,
+	Achromatopsia,
+	Deuteranomaly, // Malfunctioning M-cone (green)
+	Deuteranopia,  // Missing. M-cone (green)
+	Protanomaly,   // Malfunctioning L-cone (red)
+	Protanopia,    // Missing L-cone (red)
+	Tritanomaly,
+	Tritanopia,
 }
